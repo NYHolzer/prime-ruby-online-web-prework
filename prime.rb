5ec1ab348d7bfae153856047f78ffa8afd5fde6c
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-10 00:15:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(num)
-  numerators = (2..num).to_a
+  numerators = *((2..num)  #Array of numbers between
   
   numerators.each do |x|
     if (num % x  == 0 || num < 0 || num == 1) && num != 2
